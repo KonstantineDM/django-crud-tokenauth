0be777fe6d784d9f469836a0ab34b25625eac488
KonstantineDM <iteamallard@gmail.com>
2021-11-23 13:42:03 +0300

    changed urlpatterns according to views.py

--- crudproject/crudapp/api/urls.py
+++ crudproject/crudapp/api/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 app_name = 'crudapp'
 urlpatterns = [
-    path('create', views.api_account_create, name='create'),
-    path('<int:acc_id>', views.api_account_read, name='read'),
-    path('accounts/', views.api_account_read_all, name='read_all'),
-    path('<int:acc_id>/update', views.api_account_update, name='update'),
-    path('<int:acc_id>/delete', views.api_account_delete, name='delete'),
+    path('create', views.AccountViewset.as_view({'post': 'create'})),
+    path('<int:pk>', views.AccountViewset.as_view({'get': 'retrieve'})),
+    path('accounts/', views.AccountViewset.as_view({'get': 'list'})),
+    path('<int:pk>/update', views.AccountViewset.as_view({'put': 'update'})),
+    path('<int:pk>/delete', views.AccountViewset.as_view({'delete': 'destroy'})),
 ]
