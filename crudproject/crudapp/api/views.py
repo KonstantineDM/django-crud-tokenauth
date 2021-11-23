e1d576fb2df681d25c9a656d3f78972bbd75b5cb
KonstantineDM <iteamallard@gmail.com>
2021-11-23 13:47:07 +0300

    switched from api views to ModelViewSet - less code

--- crudproject/crudapp/api/views.py
+++ crudproject/crudapp/api/views.py
@@ -1,89 +1,41 @@
 from crudapp.api.serializers import AccountSerializer
 from crudapp.models import Account
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
-# CREATE
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated, ))
-def api_account_create(request):
-    # user = User.objects.get(username='admin')
-    user = request.user
+class AccountViewset(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated]
 
-    account = Account(creator=user)
-    serializer = AccountSerializer(account, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        account = Account(creator=request.user)
+        serializer = AccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if instance.creator != request.user:
+            return Response({'status': "You don't have permissions for this operation."})
+        else:
+            self.perform_update(serializer)
+            return Response(serializer.data)
 
-# READ
-@api_view(['GET', ])
-@permission_classes((IsAuthenticated, ))
-def api_account_read(request, acc_id):
-    try:
-        account = Account.objects.get(pk=acc_id)
-    except Account.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = AccountSerializer(account)
-    return Response(serializer.data)
-
-
-@api_view(['GET', ])
-@permission_classes((IsAuthenticated, ))
-def api_account_read_all(request):
-    accounts = Account.objects.all()
-    serializer = AccountSerializer(accounts, many=True)
-    return Response(serializer.data)
-
-
-# UPDATE
-@api_view(['PUT', ])
-@permission_classes((IsAuthenticated, ))
-def api_account_update(request, acc_id):
-    try:
-        account = Account.objects.get(pk=acc_id)
-    except Account.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    user = request.user
-    if account.creator != user:
-        return Response({'status': "You don't have permissions for this operation."})
-
-    serializer = AccountSerializer(account, data=request.data)
-    data = {}
-    if serializer.is_valid():
-        serializer.save()
-        data['status'] = 'Update completed'
-        return Response(data=data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# DELETE
-@api_view(['DELETE', ])
-@permission_classes((IsAuthenticated, ))
-def api_account_delete(request, acc_id):
-    try:
-        account = Account.objects.get(pk=acc_id)
-    except Account.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    user = request.user
-    if account.creator != user:
-        return Response({'status': "You don't have permissions for this operation."})
-
-    deletion = account.delete()
-    data = {}
-    if deletion:
-        data['status'] = 'Delete completed'
-    else:
-        data['status'] = 'Delete failed'
-    
-    return Response(data=data)
-
-
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.creator != request.user:
+            return Response({'status': "You don't have permissions for this operation."})
+        else:
+            self.perform_destroy(instance)
+            return Response(
+                {"status": "Deleted successfully"}, 
+                status=status.HTTP_200_OK
+            )
